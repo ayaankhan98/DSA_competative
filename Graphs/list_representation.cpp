0de6f7d3e86459e4a5339658d03a34023e163a43
Ayaan Khan <ayaankhan98@gmail.com>
2020-04-27 18:39:16 +0530

    connected components in Undi-graph and reachibility from city u to city v in undi-graph

--- Graphs/list_representation.cpp
+++ Graphs/list_representation.cpp
@@ -123,13 +123,11 @@ bool Graph::detectCycle()
 
 int main()
 {
-    Graph g(6);
-    g.addEdge(0, 1);
+    Graph g(4);
     g.addEdge(1, 2);
     g.addEdge(2, 3);
+    g.addEdge(3, 4);
     g.addEdge(1, 4);
-    g.addEdge(4, 5);
-    g.addEdge(3, 3);
 
     g.BFS(0);
     cout<<endl;
