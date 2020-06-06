1aacfed7eb9ed04148a6b2d6e4aa0d5c2099cea9
Ayaan Khan <ayaankhan98@gmail.com>
2020-06-07 01:29:59 +0530

    updated template

--- template.cpp
+++ template.cpp
@@ -10,20 +10,26 @@
 
 using namespace std;
 
+#define gc getchar_unlocked
 #define pb push_back
 #define mp make_pair
 #define fi first
 #define se second
 #define INF 1e9
+#define debug(x) cout<< #x << "=" << x <<endl
+#define debug2(x,y) cout<< #x << "=" << x << ", " << #y << "=" << y << endl
 #define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define clr(x) memset(x, 0, sizeof(x))
+#define PI 3.1415926535897932384626
 
 typedef long long ll;
+typedef double db;
 typedef vector<int> vi;
 typedef vector<ll> vll;
-typedef pair<int,int> pii;
-typedef map<int,int> mii;
-typedef map<char,int> mci;
-typedef double db;
+typedef pair<int, int> pii;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+typedef vector<pii> vpii;
 
 auto ONLINE = [] () {
 #ifndef ONLINE_JUDGE
