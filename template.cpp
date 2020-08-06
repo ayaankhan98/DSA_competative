b070cfe5c011a600d238a9fff21014eb80c30ba8
Ayaan Khan <ayaankhan98@gmail.com>
2020-08-06 10:31:19 +0530

    updated template

--- template.cpp
+++ template.cpp
@@ -1,30 +1,38 @@
 /**
- *
- * @Filename : template.cpp
- * @Author : Ayaan Khan
- *
- * template file
+ * @author ayaankhan98
  */
-
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <map>
-#include <set>
-#include <list>
-#include <unordered_map>
-#include <unordered_set>
-#include <functional>
 #include <algorithm>
-#include <iterator>
+#include <bitset>
+#include <cassert>
 #include <cmath>
-#include <stack>
-#include <queue>
+#include <complex>
+#include <valarray>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <deque>
 #include <fstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <cstdlib>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <deque>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <array>
+#include <climits>
+#include <limits>
+#include <memory>
+#include <bit>
+#include <sstream>
 
 using namespace std;
 
@@ -33,14 +41,18 @@ using namespace std;
 #define mp make_pair
 #define fi first
 #define se second
-#define INF 1e9
-#define debug(x) cout<< #x << "=" << x <<endl
-#define debug2(x,y) cout<< #x << "=" << x << ", " << #y << "=" << y << endl
-#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define clr(x) memset(x, 0, sizeof(x))
+#define INF ULLONG_MAX
+#define dbg(x) cout << #x << "=" << x << endl
+#define dbg2(x, y) cout << #x << "=" << x << ", " << #y << "=" << y << endl
+#define FIO                                                                    \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+#define setz(x) memset(x, 0, sizeof(x))
+#define seto(x) memset(x, 1, sizeof(x))
 #define PI 3.1415926535897932384626
 
-typedef long long ll;
+typedef int64_t ll;
 typedef double db;
 typedef vector<int> vi;
 typedef vector<ll> vll;
@@ -49,24 +61,26 @@ typedef pair<int, int> pii;
 typedef map<int, int> mii;
 typedef map<char, int> mci;
 typedef vector<pii> vpii;
+typedef vector<pair<ll, ll>> vpll;
+typedef set<int> si;
 
-auto ONLINE = [] () {
+auto ONLINE = []() {
 #ifndef ONLINE_JUDGE
-  freopen("input.txt","r",stdin);
-  freopen("output.txt","w",stdout);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
 #endif
 };
 
 void solve() {
-  // write your code here
+  /// write your code here
 }
 
 int main() {
   FIO;
   ONLINE();
-  int t;
-  cin>>t;
-  while(t--)
+  int t = 1;
+  cin >> t;
+  while (t--)
     solve();
   return 0;
 }
